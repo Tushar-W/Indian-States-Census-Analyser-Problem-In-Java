fc523898d3253647366fc5a6e0842c9bc403ea53
Tushar-W <twankhede95@gmail.com>
2020-04-30 10:43:41 +0530

    ADDED:[TC2.1]tested to check to ensure no of record matches

--- src/test/java/com/bl/censusanalyser/StateCensusAnalyserTest.java
+++ src/test/java/com/bl/censusanalyser/StateCensusAnalyserTest.java
@@ -10,6 +10,7 @@ public class StateCensusAnalyserTest {
     private static StateCensusAnalyser stateCensusAnalyser;
     private static final String INDIAN_STATES_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
     private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
+    private static final String INDIAN_STATES_CODE_CSV_FILE_PATH = "./src/test/resources/IndiaStateCode.csv";
 
     @Before
     public void initializeOject() {
@@ -34,4 +35,12 @@ public class StateCensusAnalyserTest {
             Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
         }
     }
+
+    //TC2.1
+    @Test
+    public void givenIndianStateCodeCSVFile_ShouldReturnsCorrectRecords() {
+            int noOfRecords = stateCensusAnalyser.loadIndianStateCodeData(INDIAN_STATES_CODE_CSV_FILE_PATH);
+            Assert.assertEquals(37, noOfRecords);
+    }
+
 }
