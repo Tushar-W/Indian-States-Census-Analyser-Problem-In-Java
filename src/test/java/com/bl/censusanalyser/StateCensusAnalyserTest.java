b7f6ad20923b2c532b15ecd3b24329aa2b33e96b
Tushar-W <twankhede95@gmail.com>
2020-04-30 09:50:26 +0530

    ADDED:[TC1.2]tested to check given wrong file then throw exception

--- src/test/java/com/bl/censusanalyser/StateCensusAnalyserTest.java
+++ src/test/java/com/bl/censusanalyser/StateCensusAnalyserTest.java
@@ -1,21 +1,37 @@
 package com.bl.censusanalyser;
 
+import com.bl.censusanalyserexception.CensusAnalyserException;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class StateCensusAnalyserTest {
-    public static StateCensusAnalyser stateCensusAnalyser;
-    public static final String INDIAN_STATES_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
+    private static StateCensusAnalyser stateCensusAnalyser;
+    private static final String INDIAN_STATES_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
+    private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
 
     @Before
     public void initializeOject() {
         stateCensusAnalyser = new StateCensusAnalyser();
     }
-
+    
+    //TC1.1
     @Test
     public void givenIndianStateCensusCSVFile_ShouldReturnsCorrectRecords() {
         int noOfRecords = stateCensusAnalyser.loadIndianStateCensusData(INDIAN_STATES_CENSUS_CSV_FILE_PATH);
         Assert.assertEquals(29,noOfRecords);
+    }
+
+    //TC1.2
+    @Test
+    public void givenIndiaStateCensusData_WithWrongFile_ShouldThrowException() {
+        try {
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            int noOfRecords = stateCensusAnalyser.loadIndianStateCensusData(WRONG_CSV_FILE_PATH);
+        }catch(CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
+        }
     }
 }
